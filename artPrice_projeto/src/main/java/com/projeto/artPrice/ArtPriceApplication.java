a3ca48215e84c90ffc18cbcf59e6e34b5a5ec600
Tuany Cristo <t.cristo@hotmail.com>
2024-10-07 09:44:12 -0300

    backend do projeto

---
+++ artPrice_projeto/src/main/java/com/projeto/artPrice/ArtPriceApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.artPrice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtPriceApplication.class, args);
+	}
+
+}
---
+++ artPrice_projeto/src/test/java/com/projeto/artPrice/ArtPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.artPrice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtPriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
